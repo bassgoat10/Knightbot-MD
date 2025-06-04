4ccdd88bce340359fd0268bbccde658fbb96d025
bassgoat10 <thiounebass2003@gmail.com>
2025-06-04 15:17:36 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎Bot',
   botName: "Knight Bot",
-  botOwner: 'Elhadji', // Your name
-  ownerNumber: '221786539490', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Bass_Hacker', // Your name
+  ownerNumber: '221789547996', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
